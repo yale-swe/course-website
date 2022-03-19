1d5dbd8a20c60f5350b9fcd8ea75eec5ab9bb8ff
wu-json <jason.wu2153@gmail.com>
2022-03-18 23:55:03 -0400

    feat: refactor nav

---
+++ src/components/NavigationBar/FullNavigationBar/index.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
+import Logo from '../../../assets/images/logo.png';
+import DarkModeToggle from '../../DarkModeToggle';
+import styles from './styles.module.scss';
+
+const FullNavigationBar = () => {
+    const navigate = useNavigate();
+    return (
+        <div className={styles['container']}>
+            <img className={styles['logo']} src={Logo} alt="yale-swe-logo" />
+            <div className={styles['text-routes']}>
+                <h1 onClick={() => navigate('/about')}>about us</h1>
+                <h1 onClick={() => navigate('/staff')}>staff</h1>
+                <h1 onClick={() => navigate('/projects')}>projects</h1>
+                <h1 onClick={() => navigate('/docs')}>docs</h1>
+                <h1 onClick={() => navigate('/contact')}>contact us</h1>
+                <div className={styles['toggle-container']}>
+                    <DarkModeToggle />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FullNavigationBar;
