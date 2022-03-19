24e7b30ec3ae67f49e31f4d05016ec84e8f54f89
wu-json <jason.wu2153@gmail.com>
2022-03-18 22:06:32 -0400

    feat: start navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,3 +1,16 @@
-const NavigationBar = () => {};
+import Logo from '../assets/logo.png';
+
+const NavigationBar = () => {
+    return (
+        <div>
+            <img src={Logo} alt="yale-swe-logo" />
+            <div>
+                <h1>About us</h1>
+                <h1>Staff</h1>
+                <h1>Contact us</h1>
+            </div>
+        </div>
+    );
+};
 
 export default NavigationBar;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,12 @@
+import NavigationBar from '../../components/NavigationBar';
+import styles from './styles.module.scss';
+
 const Home = () => {
-    return <></>;
+    return (
+        <div className={styles['page-container']}>
+            <NavigationBar />
+        </div>
+    );
 };
 
 export default Home;
