27a23ef9739ad28fba33840302943351667e7eaf
wu-json <jason.wu2153@gmail.com>
2022-03-18 23:56:34 -0400

    feat: side nav placeholder

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,10 +1,11 @@
 import { useWindowWidth } from '@react-hook/window-size';
 
 import FullNavigationBar from './FullNavigationBar';
+import SideNavigationBar from './SideNavigationBar';
 
 const NavigationBar = () => {
     const windowWidth = useWindowWidth();
-    return windowWidth > 1200 ? <FullNavigationBar /> : <></>;
+    return windowWidth > 1200 ? <FullNavigationBar /> : <SideNavigationBar />;
 };
 
 export default NavigationBar;
