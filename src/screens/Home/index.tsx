4acd825c98e332501275141a9e337f66b0768c1a
wu-json <jason.wu2153@gmail.com>
2022-03-19 17:28:06 -0400

    feat: homepage

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,4 @@
+import UndrawGraphic from '../../assets/images/undraw-code-thinking.svg';
 import NavigationBar from '../../components/NavigationBar';
 import styles from './styles.module.scss';
 
@@ -5,6 +6,18 @@ const Home = () => {
     return (
         <div className={styles['page-container']}>
             <NavigationBar />
+            <div className={styles['content-container']}>
+                <div className={styles['text-container']}>
+                    <h1>software engineering at yale</h1>
+                    <h3>
+                        CPSC 439/539 at Yale University. We help students learn
+                        by creating real software projects.
+                    </h3>
+                </div>
+                <div className={styles['graphic-container']}>
+                    <img src={UndrawGraphic} />
+                </div>
+            </div>
         </div>
     );
 };
