82dbd4682000183fee5eecadf4f1394eb90586c9
wu-json <jason.wu2153@gmail.com>
2022-03-18 23:43:45 -0400

    feat: add nav routes

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -11,10 +11,10 @@ const NavigationBar = () => {
             <img className={styles['logo']} src={Logo} alt="yale-swe-logo" />
             <div className={styles['text-routes']}>
                 <h1 onClick={() => navigate('/about')}>about us</h1>
-                <h1>staff</h1>
-                <h1>projects</h1>
-                <h1>docs</h1>
-                <h1>contact us</h1>
+                <h1 onClick={() => navigate('/staff')}>staff</h1>
+                <h1 onClick={() => navigate('/projects')}>projects</h1>
+                <h1 onClick={() => navigate('/docs')}>docs</h1>
+                <h1 onClick={() => navigate('/contact')}>contact us</h1>
                 <div className={styles['toggle-container']}>
                     <DarkModeToggle />
                 </div>
