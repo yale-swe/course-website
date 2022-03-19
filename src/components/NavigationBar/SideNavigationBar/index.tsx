1baf6db50a82c14b68e9df83548d3513ebbb28c4
wu-json <jason.wu2153@gmail.com>
2022-03-19 01:19:56 -0400

    feat: navbar updates

--- src/components/NavigationBar/SideNavigationBar/index.tsx
+++ src/components/NavigationBar/SideNavigationBar/index.tsx
@@ -1,9 +1,10 @@
 import { CSSProperties, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Transition } from 'react-transition-group';
 
-// import { useNavigate } from 'react-router-dom';
 import Logo from '../../../assets/images/logo.png';
-// import DarkModeToggle from '../../DarkModeToggle';
+import DarkModeToggle from '../../DarkModeToggle';
+import TEXT_ROUTES from '../TextRoutes';
 import styles from './styles.module.scss';
 
 const duration = 300;
@@ -25,17 +26,29 @@ const SideBar = ({
 }: {
     setIsOpen: (isOpen: boolean) => void;
     style?: CSSProperties;
-}) => (
-    <div className={styles['sidebar']} style={style}>
-        <i
-            className={`fa-solid fa-bars ${styles['burger']}`}
-            onClick={() => setIsOpen(false)}
-        />
-    </div>
-);
+}) => {
+    const navigate = useNavigate();
+    return (
+        <div className={styles['sidebar']} style={style}>
+            <i
+                className={`fa-solid fa-bars ${styles['burger']}`}
+                onClick={() => setIsOpen(false)}
+            />
+            <div className={styles['text-routes']}>
+                {TEXT_ROUTES.map(({ route, label }) => (
+                    <h1 onClick={() => navigate(route)} key={label}>
+                        {label}
+                    </h1>
+                ))}
+                <div className={styles['toggle-container']}>
+                    <DarkModeToggle />
+                </div>
+            </div>
+        </div>
+    );
+};
 
 const SideNavigationBar = () => {
-    // const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState<boolean>(false);
     return (
         <>
