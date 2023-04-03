d6e97d6e0df5382b151b38358f5a266f3bc6b32e
timosantonopoulos <timos.antonopoulos@gmail.com>
2023-04-03 01:18:36 -0400

    fetching

--- src/screens/ABTemplate/index.tsx
+++ src/screens/ABTemplate/index.tsx
@@ -7,8 +7,7 @@ const ABTemplate = () => {
     const html =
         '<!DOCTYPE html><html><head><title>Simple HTML Page</title></head><body><a name="abtest" href="/abc/?v=v' +
         rand.toString() +
-        '"><button>Go to Example.com</button></a></body></html>';
-    // const html = '<!DOCTYPE html><html><head><title>Simple HTML Page</title></head><body><a name="abtest" href="/abc/?v=v1"><button>Go to Example.com</button></a></body></html>';
+        '"><button>Perform Action</button></a></body></html>';
     return <div dangerouslySetInnerHTML={{ __html: html }}></div>;
 };
 
