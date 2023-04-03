b7419a1974eaf00e2db16aec4a1206ff1702daf1
timosantonopoulos <timos.antonopoulos@gmail.com>
2023-04-02 23:37:51 -0400

    fetching

--- src/screens/AB/index.tsx
+++ src/screens/AB/index.tsx
@@ -1,6 +1,9 @@
 // import { useEffect } from 'react';
 import React, { useState } from 'react';
 
+import NavigationBar from '../../components/NavigationBar';
+import styles from './styles.module.scss';
+
 // import NavigationBar from '../../components/NavigationBar';
 // import StaffContent from '../../components/StaffContent';
 // import styles from './styles.module.scss';
@@ -18,32 +21,54 @@ import React, { useState } from 'react';
 // };
 
 const AB = () => {
-    const [text, setText] = useState('Hello World');
+    const [text, setText] = useState('Enter the URL and wait for result');
+    const [value, setValue] = useState('');
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setValue(event.target.value);
+    }
 
     const handleChangeText = () => {
         setText('Checking...');
 
+        console.log(value);
         const url = 'https://misc-dev.onrender.com/check/';
         const options = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
+            // body: "resourceURL=google.com"
             body: JSON.stringify({
-                resourceURL: 'google.com'
+                resourceURL: value
             })
         };
 
         fetch(url, options)
-            // .then(response => setText(response.text()))
-            // .then(json => setText(json))
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.result) {
+                    setText(data.result);
+                } // Do something with the parsed data
+            })
             .catch(error => setText('error: ' + error));
     };
 
     return (
-        <div>
-            <p>{text}</p>
-            <button onClick={handleChangeText}>Check</button>
+        <div className={styles['page-container']}>
+            <NavigationBar />
+            <div className={styles['content-container']}>
+                {/* <h1>About</h1> */}
+                <div style={{ marginBottom: '30px' }}></div>
+                <p style={{ marginBottom: '0px', textAlign: 'center' }}>
+                    {text}
+                </p>
+                <div style={{ marginBottom: '30px' }}></div>
+                <input type="text" value={value} onChange={handleChange} />
+                <div style={{ marginBottom: '10px' }}></div>
+                <button onClick={handleChangeText}>Check URL</button>
+            </div>
         </div>
     );
 };
