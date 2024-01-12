aa028f3ef15f6757537b53bd60aecf2a303801ef
timosantonopoulos <timos.antonopoulos@gmail.com>
2024-01-12 03:41:43 -0500

    updates

--- src/screens/Projects/index.tsx
+++ src/screens/Projects/index.tsx
@@ -566,7 +566,10 @@ const Projects = () => {
             <div className={styles['content-container']}>
                 <h1>Projects</h1>
                 <div style={{ marginBottom: '30px' }}></div>
-                <h2>Fall 2023</h2>
+                <h2>Spring 2024</h2>
+                <div style={{ fontSize: '30px' }}>TBD</div>
+                <div style={{ height: '50px' }}></div>
+                <h3>Fall 2023</h3>
                 {PROJECTS_F23.map(
                     ({
                         name,
@@ -591,7 +594,6 @@ const Projects = () => {
                         />
                     )
                 )}
-                {/* <div style={{ fontSize: '30px' }}>TBD</div> */}
                 <div style={{ height: '50px' }}></div>
                 <h3>Spring 2023</h3>
                 {PROJECTS_S23.map(
