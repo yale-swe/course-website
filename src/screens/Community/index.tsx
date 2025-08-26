15ccf881c2df18da183e06a240e9e6a4fa26a40c
timosantonopoulos <timos.antonopoulos@gmail.com>
2025-08-26 13:16:21 -0400

    4390

--- src/screens/Community/index.tsx
+++ src/screens/Community/index.tsx
@@ -9,10 +9,10 @@ const Community = () => {
             <div className={styles['content-container']}>
                 <h1>Community</h1>
                 <div className={styles['vrgap']}></div>
-                <a href="https://discord.com/channels/1278062838224785541/1278062838686027776">
-                    Discord Server for Fall 2024
+                <a href="https://discord.com/channels/1409917815686692994/">
+                    Discord Server for Fall 2025
                 </a>
-                <a href="https://discord.gg/9tzv9PfJjX">(Invite Link)</a>
+                <a href="https://discord.gg/AUxgaUeccN">(Invite Link)</a>
                 <div className={styles['vrgap']}></div>
                 {/* <img width="200px" src={DiscordQRImg}></img> */}
             </div>
